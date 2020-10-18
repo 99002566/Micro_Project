23516afba102b6e2a094b29e01f16e1d605f6aba
99002566 <71494050+99002566@users.noreply.github.com>
2020-10-18 09:22:12 +0530

    Update shell1.sh

--- Shell_Scripting/shell1.sh
+++ Shell_Scripting/shell1.sh
@@ -22,10 +22,10 @@ if [ "$repo" != "Repo link" ]; then
     #echo "$err"
     make test -C $BUILD_MAKE
     FIN_VAL=`find "$BUILD_MAKE" -name "Test*.out"`
-    echo "dir = $FIN_VAL"
+    #echo "dir = $FIN_VAL"
     valgrind "./$FIN_VAL" 2> valgrinr.csv
     VALG=`grep "ERROR SUMMARY" valgrinr.csv`
-    echo "ERR = ${VALG:23:2}"
+   # echo "ERR = ${VALG:23:2}"
     echo "$name ,$email ,$repo ,$GIT_STATUS ,$BUILD_STATUS ,$err ,${VALG:23:2}" >> REPORT_SUMMARY.csv
 fi
 done < Input.csv
